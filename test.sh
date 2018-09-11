c00d8acee5b02f41e7619e77f240773bd308a130
Dejan Milosevic <dejan.milosevic@quantox.com>
2018-09-11 16:02:00 +0200

    travis

--- test.sh
+++ test.sh
@@ -4,5 +4,4 @@ fi
 if [[ "$(docker images -q nginx:latest  2> /dev/null)" == "" ]]; then
   docker pull nginx:latest
 fi
-docker volume create --name test --opt type=none --opt device=$(pwd) --opt o=bind
 docker run --user root -it --rm --net=host -w /app/test -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/app alekzonder/puppeteer:latest npm run test
