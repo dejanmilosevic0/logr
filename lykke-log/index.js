71f8dd1dc1a4a8bdaf9722be7fb65dfc27603559
Dejan Milosevic <dejan.milosevic@quantox.com>
2018-10-06 06:27:10 +0200

    update

--- lykke-log/index.js
+++ lykke-log/index.js
@@ -3,6 +3,7 @@
 import lykkeLogWebpack from './src/lykke-log-webpack.js';
 import lykkeLog from './src/lykke-log';
 
-
-module.exports.webpackPlugin = lykkeLogWebpack;
-module.exports.lykkeLog = lykkeLog;
+module.exports = {
+    webpackPlugin: lykkeLogWebpack,
+    lykkeLog: lykkeLog
+}
