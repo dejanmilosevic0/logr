fa29d9f7b755836a356c2e7f9f231f0da7b9accd
Dejan Milosevic <dejan.milosevic@quantox.com>
2018-10-17 10:17:38 +0200

    update

--- lykke-log-webpack/lib/plugin.js
+++ lykke-log-webpack/lib/plugin.js
@@ -24,9 +24,8 @@ function createTag(script) {
 class LykkeLogWebpack {
   constructor(options) {
     // this.options = normalizeOptions(options);
-    this.script = `for(var lykke_log_deffered={errors:[],console:[],console_original:{},methods:"log warn error debug info trace".split(" "),lykke_log_deffered_error_handler:function(a){var b;"SCRIPT"==a.target.tagName||"IMG"==a.target.tagName?b=a.target.src:null;"LINK"==a.target.tagName?b=a.target.href:null;lykke_log_deffered.errors.push(Error("Failed to load "+a.target.tagName+" "+b))},console_intercept:function(a){"log"==a?a="info":null;"trace"==a?a="verbose":null;lykke_log_deffered.console_original[a]=console[a];
-    console[a]=function(){var b=Array.prototype.slice.apply(arguments).join(" ");lykke_log_deffered.console_original[a]?(lykke_log_deffered.console_original[a].apply(console,arguments),lykke_log_deffered.console.push({method:a,arguments:arguments})):(lykke_log_deffered.console_original[a](b),lykke_log_deffered.console.push(b))}}},i=0;i<lykke_log_deffered.methods.length;i++)lykke_log_deffered.console_intercept(lykke_log_deffered.methods[i]);
-    window.addEventListener("error",lykke_log_deffered.lykke_log_deffered_error_handler,!0);`;
+    this.script = `for(var lykke_log_deffered={errors:[],console:[],console_original:{},methods:"log warn error debug info trace".split(" "),lykke_log_deffered_error_handler:function(a){var b;"SCRIPT"==a.target.tagName||"IMG"==a.target.tagName?b=a.target.src:null;"LINK"==a.target.tagName?b=a.target.href:null;lykke_log_deffered.errors.push(Error("Failed to load "+a.target.tagName+" "+b))},console_intercept:function(a){lykke_log_deffered.console_original[a]=console[a];console[a]=function(){var b=Array.prototype.slice.apply(arguments).join(" ");
+    lykke_log_deffered.console_original[a]?(lykke_log_deffered.console_original[a].apply(console,arguments),lykke_log_deffered.console.push({method:a,arguments:arguments})):(lykke_log_deffered.console_original[a](b),lykke_log_deffered.console.push({method:a,arguments:b}))}}},i=0;i<lykke_log_deffered.methods.length;i++)lykke_log_deffered.console_intercept(lykke_log_deffered.methods[i]);window.addEventListener("error",lykke_log_deffered.lykke_log_deffered_error_handler,!0);`;
   }
   apply(compiler) {
     //const options = this.options;
